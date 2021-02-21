945ee60af48b2700e50ad02b2fa4e4a22f2aefb3
Nir Benarous <rdt.maya@gmail.com>
2021-02-21 19:55:03 +0200

    finished the statistical similarity measures

--- Examples_code.py
+++ Examples_code.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-# from bleu_and_reverse_bleu import bleu
-# from likelihood_and_ws_likelihood import MarkovChain, liklihood, ws_likelihood
-# from discriminator_and_ws_discriminator import discriminator_model, discrimiator_score, ws_discrimiator_score
-# from reidentification_probability import reidentification_prob
-# from proportion_of_identical_diaries import identical_diaries
-from statistics import statistical_area_distribution
+from bleu_and_reverse_bleu import bleu
+from likelihood_and_ws_likelihood import MarkovChain, liklihood, ws_likelihood
+from discriminator_and_ws_discriminator import discriminator_model, discrimiator_score, ws_discrimiator_score
+from reidentification_probability import reidentification_prob
+from proportion_of_identical_diaries import identical_diaries
+from statistics import statistical_area_distribution, transitions_score, working_hours_distribution
 
 orig = pd.DataFrame(
     [
@@ -34,46 +34,51 @@ synth = pd.DataFrame(
     columns=[f"1_Hour{i}" for i in range(12)]
 ).astype(str)
 
-# ##########################################
-# ################# BLEU ###################
-#
-# print("BLEU score", bleu(synth, orig, 3, False))
-# print("Reverse BLEU score", bleu(synth, orig, 3, True))
-#
-# ##########################################
-# ############## Likelihood ################
-#
-# markov_chain_model = MarkovChain(3, orig.columns)
-# # markov_chain_model.train(orig)
-#
-# print("Likelihood score", liklihood(synth, markov_chain_model))
-# print("WS likelihood score", ws_likelihood(synth, orig, markov_chain_model))
-#
-# ##########################################
-# ############# Discriminator ##############
-#
-# d = discriminator_model()
-# d.train_model(orig, random_data_size=5, batch_size=4, epochs=1)
-#
-# print("Discriminator score", discrimiator_score(d, synth))
-# print("WS Discriminator score", ws_discrimiator_score(d, synth, orig))
-#
-# ##########################################
-# ############ Reidentification ############
-#
-# print("Reidentification probability score", reidentification_prob(synth, orig, 3, 3))
-#
-# ##########################################
-# ############ Reidentification ############
-#
-# print("Proportion of identical diaries", identical_diaries(synth, orig))
-#
-# ###########################################
-# ###### statistical area distribution ######
-#
-# print("Statistical area distribution", statistical_area_distribution(synth, orig))
+##########################################
+################# BLEU ###################
+
+print("BLEU score", bleu(synth, orig, 3, False))
+print("Reverse BLEU score", bleu(synth, orig, 3, True))
+
+##########################################
+############## Likelihood ################
+
+markov_chain_model = MarkovChain(3, orig.columns)
+# markov_chain_model.train(orig)
+
+print("Likelihood score", liklihood(synth, markov_chain_model))
+print("WS likelihood score", ws_likelihood(synth, orig, markov_chain_model))
+
+##########################################
+############# Discriminator ##############
+
+d = discriminator_model()
+d.train_model(orig, random_data_size=5, batch_size=4, epochs=1)
+
+print("Discriminator score", discrimiator_score(d, synth))
+print("WS Discriminator score", ws_discrimiator_score(d, synth, orig))
+
+##########################################
+############ Reidentification ############
+
+print("Reidentification probability score", reidentification_prob(synth, orig, 3, 3))
+
+##########################################
+############ Reidentification ############
+
+print("Proportion of identical diaries", identical_diaries(synth, orig))
 
 ###########################################
 ###### statistical area distribution ######
 
 print("Statistical area distribution", statistical_area_distribution(synth, orig))
+
+###########################################
+############ Transition matrix ############
+
+print("Transition matrix", transitions_score(synth, orig))
+
+###########################################
+######### Work hours distribution #########
+
+print("Working hours distribution", working_hours_distribution(synth, orig, [f"1_Hour{i}" for i in range(12)], 1))
--- statistics.py
+++ statistics.py
@@ -64,21 +64,158 @@ def statistical_area_distribution(synthetic_data, original_data):
     return kl_divergence(merged.share_x, merged.share_y)
 
 
-# def get_transition_data(data):
-#     melted_data = pd.melt(data.reset_index(), id_vars=['data', 'index'])
-#     melted_data = melted_data[melted_data.value != -1]
-#
-#     melted_data['time'] = melted_data.variable.apply(lambda x: order_cols_weekday.index(x))
-#     melted_data['day'] = melted_data.variable.apply(lambda x: int(x.split('_')[0]))
-#     melted_data.sort_values('time', inplace=True)
-#     melted_data['new_time'] = melted_data.groupby(['data', 'index', 'day']).cumcount() + 1
-#     melted_data['new_time_plus'] = melted_data['new_time'] + 1
-#
-#     melted_data_merge = melted_data.merge(melted_data, left_on=['data', 'index', 'new_time_plus'],
-#                                           right_on=['data', 'index', 'new_time'])
-#     melted_data_merge = melted_data_merge[
-#         (melted_data_merge.value_x != melted_data_merge.value_y) & (melted_data_merge.day_x == melted_data_merge.day_y)]
-#     grouped = melted_data_merge.groupby(['data', 'value_x', 'value_y', 'variable_x']).time_x.count().reset_index()
-#     grouped.columns = ['data', 'from', 'to', 'start_time', 'cnt']
-#
-#     return grouped
+def get_transition_data(data):
+    """
+    Create transition table
+
+    Parameters
+    ----------
+    data: pandas.DataFrame
+
+    Returns
+    -------
+    transfers: pandas.DataFrame
+    """
+    melted_data = data.copy()
+    cols = melted_data.columns.to_list()
+
+    # melt data
+    melted_data.index.rename('User', inplace=True)
+    melted_data = pd.melt(melted_data.reset_index(), id_vars=['User'], var_name='day_hour', value_name='location')
+
+    # remove transitions
+    melted_data = melted_data[melted_data.location != -1]
+
+    # create index column for each user
+    melted_data['time'] = melted_data.day_hour.apply(lambda x: cols.index(x))
+    melted_data['day'] = melted_data.day_hour.apply(lambda x: int(x.split('_')[0]))
+    melted_data.sort_values('time', inplace=True)
+
+    # since we removed the traveling time we need the time column to fill the gaps
+    melted_data['time'] = melted_data.groupby(['User', 'day']).cumcount() + 1
+    melted_data['time_plus'] = melted_data['time'] + 1
+
+    # merge the time with time plus to get the next location for every location
+    transfers = melted_data.merge(melted_data, left_on=['User', 'time_plus'], right_on=['User', 'time'])
+
+    # keep only times when user moves
+    transfers = transfers[
+        (transfers.location_x != transfers.location_y) &
+        (transfers.day_x == transfers.day_y)
+    ]
+
+    # count the number of transfers by departure (location_x) and arival (location_y) locations
+    transfers = transfers.groupby(['location_x', 'location_y', 'day_hour_x']).time_x.count().reset_index()
+    transfers.columns = ['depart', 'arrive', 'start_time', 'cnt']
+    transfers['share'] = transfers.cnt / transfers.cnt.sum()
+
+    return transfers
+
+
+def transitions_score(synthetic_data, original_data):
+    """
+    Get the transitions of the synthetic data and original data.
+    Than calculate the kl divergence between them.
+
+    Parameters
+    ----------
+    synthetic_data: pandas.DataFrame
+        synthetic data
+    original_data: pandas.DataFrame
+        original data
+
+    Returns
+    -------
+    score: float
+    """
+
+    orig_transitions = get_transition_data(original_data)
+    synth_transitions = get_transition_data(synthetic_data)
+
+    merged = (
+        orig_transitions
+        .merge(synth_transitions, on=['depart', 'arrive', 'start_time'], how='outer')
+        .fillna(0.00000001)
+    )
+    return kl_divergence(merged.share_x, merged.share_y)
+
+
+def user_type(locations, working_times):
+    """
+    Function determines if user is worker or not
+
+    Parameters
+    ----------
+    locations: pandas.Series
+        sequence of locations
+    working_times: list
+        list of columns of working days
+
+    Returns
+    -------
+    is_worker: bool
+        1 if worker
+    """
+    if locations[working_times].value_counts().iloc[1] / len(working_times) >= 0.2:
+        return 1
+    else:
+        return 0
+
+
+def working_hours(data, working_times, number_of_work_days):
+    """
+    Calculate working hour for each user if the user is a worker else working hours are 0
+
+    Parameters
+    ----------
+    data: pandas.DataFrame
+    working_times: list
+        list of columns of working days
+    number_of_work_days: int
+        number of working days in the data
+
+    Returns
+    -------
+    working_hours: pandas.DataFrame
+    """
+    work_hours = data.copy()
+    work_hours['is_worker'] = work_hours.apply(lambda x: user_type(x, working_times), axis=1)
+    work_hours['work_place'] = work_hours.apply(
+        lambda x: None if x.is_worker == 0 else x[working_times].value_counts().index[1], axis=1)
+    work_hours['work_hours'] = work_hours.apply(
+        lambda x: 0 if x.is_worker == 0 else int(len([val for val in x[working_times] if val == x.work_place]) / number_of_work_days),
+        axis=1
+    )
+
+    work_hours_distribution = work_hours.groupby('work_hours').is_worker.count().reset_index()
+    work_hours_distribution.rename(columns={'is_worker': 'cnt'}, inplace=True)
+    work_hours_distribution['share'] = work_hours_distribution.cnt / work_hours_distribution.cnt.sum()
+
+    return work_hours_distribution
+
+
+def working_hours_distribution(synthetic_data, original_data, working_times, number_of_work_days):
+    """
+    Get the working hours distribution of the synthetic data and original data.
+    Than calculate the kl divergence between them.
+
+    Parameters
+    ----------
+    synthetic_data: pandas.DataFrame
+        synthetic data
+    original_data: pandas.DataFrame
+        original data
+
+    Returns
+    -------
+    score: float
+    """
+    orig_working_hours = working_hours(original_data, working_times, number_of_work_days)
+    synth_working_hours = working_hours(synthetic_data, working_times, number_of_work_days)
+
+    merged = (
+        orig_working_hours
+        .merge(synth_working_hours, on=['work_hours'], how='outer')
+        .fillna(0.00000001)
+    )
+    return kl_divergence(merged.share_x, merged.share_y)
